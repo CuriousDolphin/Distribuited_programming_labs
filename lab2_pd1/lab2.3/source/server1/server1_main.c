5acc075ea0a922e484711e38e5de60bb25f8abe9
ivan m <isnob46@gmail.com>
2019-04-11 08:47:12 -0700

    start 2.3

--- lab2_pd1/lab2.3/source/server1/server1_main.c
+++ lab2_pd1/lab2.3/source/server1/server1_main.c
@@ -3,10 +3,73 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <ctype.h>
+#include <stdio.h>
 
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+#include <rpc/xdr.h>
+
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+
+#include "../errlib.h"
+#include "../sockwrap.h"
+#define LISTENQ 15
+#define MAXBUFL 255
+
+
+#define MAX_UINT16T 0xffff
+
+#ifdef TRACE
+#define trace(x) x
+#else
+#define trace(x)
+#endif
 char *prog_name;
 
 int main (int argc, char *argv[])
 {
+	int id_socket;
+	int port;
+	struct sockaddr_in servaddr, cliaddr;
+	socklen_t cliaddrlen = sizeof(cliaddr);
+	prog_name = argv[0];
+
+	if (argc!=2 || argv[1]<0){
+		err_quit ("usage: %s need <port>\n ", prog_name);
+	}
+	
+	port=atoi(argv[1]);
+	id_socket = Socket(AF_INET, SOCK_STREAM, 0);
+
+	memset(&servaddr, 0, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_port = htons(port); 
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+	Bind(id_socket, (SA*) &servaddr, sizeof(servaddr));
+
+	trace ( err_msg("(%s) socket created",prog_name) );
+	trace ( err_msg("(%s) listening on %s:%u", prog_name, inet_ntoa(servaddr.sin_addr), ntohs(servaddr.sin_port)) );
+	Listen(id_socket, LISTENQ);
+	int connection;
+	while(1){
+		trace( err_msg ("(%s) waiting for connections ...", prog_name) );
+		connection=Accept(id_socket, (SA*) &cliaddr, &cliaddrlen);
+		if(connection>0){
+			trace ( err_msg("(%s) - new connection from client %s:%u", prog_name, inet_ntoa(cliaddr.sin_addr), ntohs(cliaddr.sin_port)) );
+
+		}	
+
+	}
 	return 0;
 }
+
