e07d9c34a8ccf77140e55b75b46c6e46ba932c55
isnob46 <isnob46@gmail.com>
2019-04-26 10:03:20 -0700

    code formatter

--- lab2_pd1/lab2.3/source/client1/client1_main.c
+++ lab2_pd1/lab2.3/source/client1/client1_main.c
@@ -4,27 +4,20 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <ctype.h>
-
-
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <sys/stat.h>
 #include <rpc/xdr.h>
-
 #include <string.h>
 #include <time.h>
 #include <unistd.h>
-
 #include "../errlib.h"
 #include "../sockwrap.h"
 #define LISTENQ 15
 #define MAXBUFL 50
-
-
 #define MAX_UINT16T 0xffff
-
 #ifdef TRACE
 #define trace(x) x
 #else
@@ -32,7 +25,7 @@
 #endif
 char *prog_name;
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int id_socket;
 	int port;
@@ -41,106 +34,113 @@ int main (int argc, char *argv[])
 	char request[MAXBUFL];
 	char *command_buf;
 	char *file_buf;
-	struct sockaddr_in  cliaddr;
+	struct sockaddr_in cliaddr;
 	socklen_t cliaddrlen = sizeof(cliaddr);
-	command_buf=calloc(6,sizeof(char));
-	//char* buf;
-	//buf=malloc(5*sizeof(char));
+	command_buf = calloc(6, sizeof(char));
 	prog_name = argv[0];
-	if ( argc<3 ){
-		err_quit ("wrong params usage: %s need <address> <port>\n ", prog_name);
+	if (argc < 3)
+	{
+		err_quit("wrong params usage: %s need <address> <port>\n ", prog_name);
 	}
-	
-	port=atoi(argv[2]);
-    addr=argv[1];
+
+	port = atoi(argv[2]);
+	addr = argv[1];
 	id_socket = Socket(AF_INET, SOCK_STREAM, 0);
 	/* CONFIGURAZIONE SOCKET */
-    cliaddr.sin_family = AF_INET; 
-    cliaddr.sin_port = htons(port); /* CONVERSIONE NETWORK ORDER SHORT*/
-	res_addr = inet_aton(addr,&cliaddr.sin_addr); /* inserimento indirizzo da stringa dotted */
-    if(res_addr==0){
-        printf("indirizzo non valido inet_aton() failed");
-    }
-	Connect(id_socket,(struct sockaddr*)&cliaddr,cliaddrlen);
+	cliaddr.sin_family = AF_INET;
+	cliaddr.sin_port = htons(port);								 /* CONVERSIONE NETWORK ORDER SHORT*/
+	res_addr = inet_aton(addr, &cliaddr.sin_addr); /* inserimento indirizzo da stringa dotted */
+	if (res_addr == 0)
+	{
+		printf("indirizzo non valido inet_aton() failed");
+	}
+	Connect(id_socket, (struct sockaddr *)&cliaddr, cliaddrlen);
 	int i;
-	
-	for(i=3;i<argc;i++){
-		strcpy(request,"");
-		strcat(request,"GET ");
-		strcat(request,argv[i]);
-		strcat(request,"\r\n");
-		//printf("\t sending: %s\n",request);
-		Send(id_socket,request,strlen(request),0);
-		 struct timeval tval;
-        fd_set cset;              //insieme di socket su cui agisce la SELECT
-        FD_ZERO(&cset);          //azzero il set
-        FD_SET(id_socket,&cset); //ASSOCIO IL SOCKET ALL'INSIEME
-        int time=10;
-        tval.tv_sec=time; tval.tv_usec=0; //imposto il tempo nell astruttura
-        int res_sel;
-        /* SELECT */
-        res_sel=select(FD_SETSIZE, &cset,NULL,NULL,&tval);
-        if(res_sel == -1){
-            printf("select() failed");
-            return -1;
-        }
-        if(res_sel>0){
-			//strcpy(command_buf,"");
+
+	for (i = 3; i < argc; i++) /* ITERAZIONE NUMERO ARGOMENTI */
+	{
+		strcpy(request, "");
+		strcat(request, "GET ");
+		strcat(request, argv[i]);
+		strcat(request, "\r\n");
+		Send(id_socket, request, strlen(request), 0);
+		struct timeval tval;
+		fd_set cset;							//insieme di socket su cui agisce la SELECT
+		FD_ZERO(&cset);						//azzero il set
+		FD_SET(id_socket, &cset); //ASSOCIO IL SOCKET ALL'INSIEME
+		int time = 10;
+		tval.tv_sec = time;
+		tval.tv_usec = 0; //imposto il tempo nell astruttura
+		int res_sel;
+		/* SELECT */
+		res_sel = select(FD_SETSIZE, &cset, NULL, NULL, &tval);
+		if (res_sel == -1)
+		{
+			printf("select() failed");
+			return -1;
+		}
+		if (res_sel > 0)
+		{
 			uint32_t len;
 			uint32_t timest;
 			uint32_t timestamp;
 			uint32_t file_len;
-			
-            Recv(id_socket,command_buf,5,0); /* COMMAND */
-            if(strcmp(command_buf,"+OK\r\n") == 0 ){
-				//len=0;
+
+			Recv(id_socket, command_buf, 5, 0); /* COMMAND */
+			if (strcmp(command_buf, "+OK\r\n") == 0)
+			{
 				printf("\t--Received Response OK");
-				Recv(id_socket,&len,4,0); /* LUNGHEZZA */
-				file_len=ntohl(len);
-				ssize_t file_size=file_len;
-				printf("\t--Received file len: '%u' byte",file_len);
+				Recv(id_socket, &len, 4, 0); /* LUNGHEZZA */
+				file_len = ntohl(len);
+				ssize_t file_size = file_len;
+				printf("\t--Received file len: '%u' byte", file_len);
 				fflush(stdout);
-				file_buf=malloc(file_size*sizeof(char)); /* ALLOCAZIONE DINAMICA, FREE PIU GIU */
-				char *start_memory=file_buf;
-				 /* RICEZIONE FILE */
-				ssize_t nread; size_t nleft;
-				 for(nleft=file_len; nleft >0 ;){
-					nread=Recv(id_socket,file_buf,nleft,0);
-					if(nread > 0){
-						nleft -=nread;
+				file_buf = malloc(file_size * sizeof(char)); /* ALLOCAZIONE DINAMICA, FREE PIU GIU */
+				char *start_memory = file_buf;
+				/* RICEZIONE FILE */
+				ssize_t nread;
+				size_t nleft;
+				for (nleft = file_len; nleft > 0;)
+				{
+					nread = Recv(id_socket, file_buf, nleft, 0);
+					if (nread > 0)
+					{
+						nleft -= nread;
 						file_buf += nread;
 					}
-				} 	  	
-				file_buf=start_memory;	
-				FILE* F;							
-				F=fopen(argv[i],"w");
-				if(F==NULL){
+				}
+				file_buf = start_memory;
+				FILE *F;
+				F = fopen(argv[i], "w");
+				if (F == NULL)
+				{
 					printf("errore salvataggio file");
 					fflush(stdout);
 					return -1;
-				}  
-
-				fwrite(file_buf,1,file_size,F);
-				fclose(F); 
-				
+				}
+				fwrite(file_buf, 1, file_size, F);
+				fclose(F);
 				struct stat st;
-				stat(argv[i],&st) ;
-				Recv(id_socket,&timest,4,0);
-				timestamp=htonl(timest);	
-				printf("\n\t---'%s' downloaded %ld byte",argv[i],st.st_size);
-				printf("\n\t--Received  timestamp: %u  \n\n",timestamp);	
-				free(file_buf); 
-			}else{
+				stat(argv[i], &st);
+				Recv(id_socket, &timest, 4, 0);
+				timestamp = htonl(timest);
+				printf("\n\t---'%s' downloaded %ld byte", argv[i], st.st_size);
+				printf("\n\t--Received  timestamp: %u  \n\n", timestamp);
+				free(file_buf);
+			}
+			else
+			{
 				printf("\t--Server error\n");
 				fflush(stdout);
 				close(id_socket);
 				break;
-			}	
-            
-        }else{
-            printf("---timeout exceded %d seconds\n",time);
-            close(id_socket);
-        }
+			}
+		}
+		else
+		{
+			printf("---timeout exceded %d seconds\n", time);
+			close(id_socket);
+		}
 	}
 	free(command_buf);
 	close(id_socket);
--- lab2_pd1/lab2.3/source/server1/server1_main.c
+++ lab2_pd1/lab2.3/source/server1/server1_main.c
@@ -26,7 +26,6 @@
 #define LISTENQ 15
 #define MAXBUFL 50
 #define MAXBUFF 100
-#define MAXRES 500000 /* 100mb */
 #define MAX_UINT16T 0xffff
 
 #ifdef TRACE
@@ -36,7 +35,7 @@
 #endif
 char *prog_name;
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int id_socket;
 	int port;
@@ -48,119 +47,118 @@ int main (int argc, char *argv[])
 	char response[MAXBUFF]; /* command for reply ..ok err */
 	char size[4];
 	char timestamp[4];
-	char err[7];   /*stringa errore*/		
-	char* start_memory; // mi serve per ripristinare il puntatore
-	strcpy(err,"-ERR\r\n");
+	char err[7];				/*stringa errore*/
+	char *start_memory; // mi serve per ripristinare il puntatore
+	strcpy(err, "-ERR\r\n");
 
-	if (argc!=2 || argv[1]<0){
-		err_quit ("usage: %s need <port>\n ", prog_name);
+	if (argc != 2 || argv[1] < 0)
+	{
+		err_quit("usage: %s need <port>\n ", prog_name);
 	}
-	
-	port=atoi(argv[1]);
+
+	port = atoi(argv[1]);
 	id_socket = Socket(AF_INET, SOCK_STREAM, 0);
 
 	memset(&servaddr, 0, sizeof(servaddr));
 	servaddr.sin_family = AF_INET;
-	servaddr.sin_port = htons(port); 
+	servaddr.sin_port = htons(port);
 	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
 
-	Bind(id_socket, (SA*) &servaddr, sizeof(servaddr));
+	Bind(id_socket, (SA *)&servaddr, sizeof(servaddr));
 
-	trace ( err_msg("(%s) socket created",prog_name) );
-	trace ( err_msg("(%s) listening on %s:%u", prog_name, inet_ntoa(servaddr.sin_addr), ntohs(servaddr.sin_port)) );
+	trace(err_msg("(%s) socket created", prog_name));
+	trace(err_msg("(%s) listening on %s:%u", prog_name, inet_ntoa(servaddr.sin_addr), ntohs(servaddr.sin_port)));
 	Listen(id_socket, LISTENQ);
 	int connection;
-	
-	while(1){
-		trace( err_msg ("(%s) waiting for connections ...", prog_name) );
-		connection=accept(id_socket, (SA*) &cliaddr, &cliaddrlen);
-		trace ( err_msg("(%s) - new connection from client %s:%u", prog_name, inet_ntoa(cliaddr.sin_addr), ntohs(cliaddr.sin_port)) );
 
-		int exit_condition=0;
-			while(exit_condition==0){
-					
-					char get_buf[4]="";	
-					int n_read =recv(connection,buf,MAXBUFL,0);
-					int n_arg=0;
-					if (n_read<=0){
-						exit_condition=1;
-						break;
-					}
-					n_arg=sscanf(buf,"%s %s",get_buf,file_name) ;
-					printf("\n\t--file name: %s,\n\t--command: %s \n",file_name,get_buf);
-					if(n_arg == 2 && strcmp(get_buf,"GET")==0){
-						struct stat st;
-						stat(file_name,&st);
-						uint32_t len=htonl(st.st_size);
-						uint32_t tim=htonl(st.st_mtime);
-						FILE *F;
-						F=fopen(file_name,"r");  	/*apertura FILE */
+	while (1)
+	{
+		trace(err_msg("(%s) waiting for connections ...", prog_name));
+		connection = accept(id_socket, (SA *)&cliaddr, &cliaddrlen);
+		trace(err_msg("(%s) - new connection from client %s:%u", prog_name, inet_ntoa(cliaddr.sin_addr), ntohs(cliaddr.sin_port)));
 
-						if(F==NULL){
-							printf("\n\tERROR FILE NOT FOUND %s\n",file_name);
-							fflush(stdout);
-							Send(connection,err,strlen(err),0);
-						
-							close(connection);
-							exit_condition=1;
-							break;
-							
-						}else{ 	
-							char* file_buf;
-							
-	
-							
-							printf("\n\t--File opened %s",file_name);					/* LETTURA FILE */
-							fflush(stdout);
-	
-							rewind(F);
-							file_buf=malloc(st.st_size*sizeof(char)); /* allocazione dinamica */
-							int bytes_read=0;							
-							bytes_read= fread( file_buf, 1, st.st_size, F );							
-  							printf( "\n\t--Bytes read: %d (previsti: %ld)\n", bytes_read,st.st_size);	
-							fflush(stdout);
-							fclose(F);
-							strcpy(response,"");
-							
-							sprintf(timestamp,"%u",ntohl(st.st_mtime));
-							strcat(response,"+OK\r\n");
-							Send(connection,response,strlen(response),0); /* +ok */
-							Send(connection,&len,sizeof(len),0);
-							start_memory=file_buf; /* IMPORTANTE */		  /*dimensione*/
-							size_t nleft; ssize_t nwritten=0;			
-							for(nleft=bytes_read;nleft >0;){
-								nwritten=send(connection,file_buf,nleft,0); /*FILE */
-								if(nwritten <=0 ){/*ERRORE*/
-									//	return (nwritten);
-								}else{
-									nleft -=nwritten;
-									file_buf +=nwritten;
-								}
-
-							}
-							file_buf=start_memory; /* RIPORTO IL PUNTATORE ALL'INIZIO */
-							Send(connection,&tim,4,0);	
-							printf("\n\t--sended %s (%ld) \n\n",file_name,nwritten);
-							free(file_buf);
-							
-						}				
-					}else{
-						
-						Send(connection,err,strlen(err),0);
-						printf("\n\t--error number argument (sscanf)");
-						exit_condition=1;
-						close(connection);
-					}
-				
+		int exit_condition = 0;
+		while (exit_condition == 0)
+		{
+			char get_buf[4] = "";
+			int n_read = recv(connection, buf, MAXBUFL, 0);
+			int n_arg = 0;
+			if (n_read <= 0)
+			{
+				exit_condition = 1;
+				break;
 			}
+			n_arg = sscanf(buf, "%s %s", get_buf, file_name);
+			printf("\n\t--file name: %s,\n\t--command: %s \n", file_name, get_buf);
+			if (n_arg == 2 && strcmp(get_buf, "GET") == 0)
+			{
+				struct stat st;
+				stat(file_name, &st);
+				uint32_t len = htonl(st.st_size);
+				uint32_t tim = htonl(st.st_mtime);
+				FILE *F;
+				F = fopen(file_name, "r"); /*apertura FILE */
+				if (F == NULL)
+				{
+					printf("\n\tERROR FILE NOT FOUND %s\n", file_name);
+					fflush(stdout);
+					Send(connection, err, strlen(err), 0);
 
-			
-		sprintf(size,"any");
-		sprintf(timestamp,"any");
-		if(exit_condition==1)
-			close(connection); 
+					close(connection);
+					exit_condition = 1;
+					break;
+				}
+				else
+				{
+					char *file_buf;
+					printf("\n\t--File opened %s", file_name); /* LETTURA FILE */
+					fflush(stdout);
+					rewind(F);
+					file_buf = malloc(st.st_size * sizeof(char)); /* allocazione dinamica */
+					int bytes_read = 0;
+					bytes_read = fread(file_buf, 1, st.st_size, F);
+					printf("\n\t--Bytes read: %d (previsti: %ld)\n", bytes_read, st.st_size);
+					fflush(stdout);
+					fclose(F);
+					strcpy(response, "");
+					sprintf(timestamp, "%u", ntohl(st.st_mtime));
+					strcat(response, "+OK\r\n");
+					Send(connection, response, strlen(response), 0); /* +ok */
+					Send(connection, &len, sizeof(len), 0);
+					start_memory = file_buf; /* IMPORTANTE */ /*dimensione*/
+					size_t nleft;
+					ssize_t nwritten = 0;
+					for (nleft = bytes_read; nleft > 0;)
+					{
+						nwritten = send(connection, file_buf, nleft, 0); /*FILE */
+						if (nwritten <= 0)
+						{ /*ERRORE*/
+							//	return (nwritten);
+						}
+						else
+						{
+							nleft -= nwritten;
+							file_buf += nwritten;
+						}
+					}
+					file_buf = start_memory; /* RIPORTO IL PUNTATORE ALL'INIZIO */
+					Send(connection, &tim, 4, 0);
+					printf("\n\t--sended %s (%ld) \n\n", file_name, nwritten);
+					free(file_buf);
+				}
+			}
+			else
+			{
+				Send(connection, err, strlen(err), 0);
+				printf("\n\t--error number argument (sscanf)");
+				exit_condition = 1;
+				close(connection);
+			}
+		}
+		sprintf(size, "any");
+		sprintf(timestamp, "any");
+		if (exit_condition == 1)
+			close(connection);
 	}
-	
 	return 0;
 }
-
